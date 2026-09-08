0f0539c89ddafe143c84c7fa05a09d153565e0eb
douglassouza <97610500+douglassnake@users.noreply.github.com>
2026-09-08 17:10:19 -0300

    chore: cache radar zoom controls

--- site/sw.js
+++ site/sw.js
@@ -1,6 +1,6 @@
-const CACHE = 'radar-gm-v14-osm-basemap';
+const CACHE = 'radar-gm-v15-radar-zoom';
 const SHELL = [
-  './', './index.html', './styles.css', './v4.css', './visual.css', './app.js', './cptec.js', './v4.js', './v5.js', './visual.js', './data/guardamor/v5.json', './manifest.webmanifest',
+  './', './index.html', './styles.css', './v4.css', './visual.css', './app.js', './cptec.js', './v4.js', './v5.js', './visual.js', './zoom.js', './data/guardamor/v5.json', './manifest.webmanifest',
   './icons/icon-180.png', './icons/icon-192.png', './icons/icon-512.png'
 ];
 
